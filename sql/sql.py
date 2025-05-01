6ff2d96470f69d7583c8853497479e56231415ba
Alfian Murfidya Rasyad <alfian.rasyad@binus.ac.id>
2025-05-01 20:27:49 +0700

    Quick fix

--- sql/sql.py
+++ sql/sql.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect("hospital_project.db")
 cursor = conn.cursor()
 
-with open("hospital_project.sql", "r") as f:
+with open("sql/hospital_project.sql", "r") as f:
     sql_script = f.read()
 
 cursor.executescript(sql_script)
